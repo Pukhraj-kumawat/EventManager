890327ef46e843eacb82ea0b09cb601550cf94b3
Pukhraj kumawat <pukhrajkumawat048@gmail.com>
2023-10-16 18:01:31 +0530

    done

--- EventManager/settings.py
+++ EventManager/settings.py
@@ -12,19 +12,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 env = environ.Env(
-    DEBUG = (bool,False)
+    DEBUG = (bool,True)
 )
 
 environ.Env.read_env(BASE_DIR / 'EventManager/.env')
 
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_SIGNATURE_NAME = env('AWS_S3_SIGNATURE_NAME')
-AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
-AWS_S3_FILE_OVERWRITE = env('AWS_S3_FILE_OVERWRITE')
-AWS_DEFAULT_ACL = env('AWS_DEFAULT_ACL')
-AWS_S3_VERIFY = env('AWS_S3_VERIFY')
+AWS_STORAGE_BUCKET_NAME="eventmanagementbucket"
+AWS_S3_SIGNATURE_NAME="s3v4"
+AWS_S3_REGION_NAME="ap-south-1"
+AWS_S3_FILE_OVERWRITE=False
+AWS_DEFAULT_ACL=None
+AWS_S3_VERIFY=False
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
