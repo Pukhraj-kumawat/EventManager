a1d1f75671fd6a328f3e8fbc5d6fb14ae53c0d8a
Pukhraj kumawat <pukhrajkumawat048@gmail.com>
2023-10-17 09:12:10 +0530

    added

--- EventManager/settings.py
+++ EventManager/settings.py
@@ -79,8 +79,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'livereload',
+    'django.contrib.messages',    
     'django.contrib.staticfiles',
     'customer',
     'whitenoise',
@@ -100,7 +99,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',    
-    'livereload.middleware.LiveReloadScript',
     
 ]
 
