5ce603e16c6c83e7b6f6fcf8c97bbf08a36ceb64
Pukhraj kumawat <pukhrajkumawat048@gmail.com>
2024-07-01 11:48:53 +0530

    some changes

--- EventPlanner/admin.py
+++ EventPlanner/admin.py
@@ -3,8 +3,12 @@ from .models import Category,Event,UserProfile,Venue,Image
 
 # Register your models here.
 
+class customUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'user_type','otp_verified')
+
+
 admin.site.register(Event)
 admin.site.register(Category)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile,customUserAdmin)
 admin.site.register(Venue)
 admin.site.register(Image)
